db2bd997e4df25f6f61b3239b69163bbd3f23773
wangshubing1 <wang_shubing@126.com>
2019-07-24 17:00:05 +0800

    ADD module template

---
+++ flink-learning-template/src/main/java/TestJava.java
@@ -0,0 +1,11 @@
+/**
+ * @Author: king
+ * @Date: 2019-07-24
+ * @Desc: TODO
+ */
+
+public class TestJava {
+    public static void main(String[] args) {
+        System.out.println("hello java");
+    }
+}
---
+++ flink-learning-template/src/main/scala/TestScala.scala
@@ -0,0 +1,12 @@
+/**
+  * @Author: king
+  * @Date: 2019-07-24
+  * @Desc: TODO 
+  */
+
+object TestScala {
+  def main(args: Array[String]): Unit = {
+    println("hello scala")
+  }
+
+}
